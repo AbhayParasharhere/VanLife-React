e83fdfc5fb7ee9728b8a7152e5741aa123990dea
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2023-10-18 18:09:03 -0700

    Complete all vans page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "/src/components/Home";
 import About from "/src/components/About";
 import Header from "/src/components/Header";
 import Footer from "/src/components/Footer";
 import VansListPage from "/src/components/VansListPage";
+import VanDetail from "/src/components/VanDetail";
 import "./server";
 
 export default function App() {
@@ -13,9 +14,10 @@ export default function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/vans" element={<VansListPage />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<VansListPage />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/components/VansListPage/index.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./styles.modules.css";
+
+const VansListPage = () => {
+  const [pageContent, setPageContent] = useState(null);
+  useEffect(() => {
+    const getResponse = async () => {
+      const response = await fetch("/api/vans");
+      const data = await response.json();
+
+      const lengthCheck = data.vans.length;
+      if (!response.ok) {
+        setPageContent(
+          <div className="error">
+            Error fetching data from the API. Please try again later.
+          </div>
+        );
+      }
+      if (lengthCheck === 0) {
+        setPageContent(
+          <div className="error">No items are currently listed.</div>
+        );
+      }
+
+      console.log(data, data.vans.length);
+      const styleTag = {
+        simple: {
+          backgroundColor: "#E17654",
+        },
+        rugged: {
+          backgroundColor: "#115E59",
+        },
+        luxury: {
+          backgroundColor: "#161616",
+        },
+      };
+      setPageContent(
+        data.vans.map((van) => {
+          return (
+            <div className="vanCardContainer" key={van.id}>
+              <Link to={`vans/${van.id}`} className="vanLink">
+                <img src={van.imageUrl} className="vanImage" alt={van.name} />
+                <div className="vanCardDetail">
+                  <div className="vanName">{van.name}</div>
+                  <div className="vanPrice">
+                    ${van.price}
+                    <div className="perDay">/day</div>
+                  </div>
+                </div>
+                <div
+                  className="vanTypeTag"
+                  style={{ ...styleTag[`${van.type}`] }}
+                >
+                  {van.type}
+                </div>
+              </Link>
+            </div>
+          );
+        })
+      );
+    };
+    getResponse();
+  }, []);
+  return (
+    <div>
+      <div className="vanPageHeading">Explore our van options.</div>
+      <div className="vansListContainer">{pageContent}</div>
+    </div>
+  );
+};
+
+export default VansListPage;
